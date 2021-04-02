6517ae18fb87c3aa92d56f050e1ff6fff335bb2b
Andreas Oldeskog <andreas.oldeskog@pricerunner.com>
2021-04-02 20:34:58 +0200

    work

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import useInterval from "./utils/hooks/useInterval";
 function App() {
     const nextDepLumabryggan = getNextDeparture("lumabryggan");
     const nextDepBarnängen = getNextDeparture("barnängen");
+    const nextDepHenriksdal = getNextDeparture("henriksdal");
     const forceUpdate = useForceUpdate();
 
     useInterval(() => {
@@ -15,20 +16,30 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src="/android-chrome-192x192.png" />
+                <img
+                    src={process.env.PUBLIC_URL + "/android-chrome-192x192.png"}
+                />
                 <h1>Sjöstadstrafiken</h1>
 
                 <p>
-                    Båten går från Lumabryggan{" "}
+                    Båten går från <strong>Lumabryggan</strong>{" "}
                     {relativeTime(nextDepLumabryggan)}
                     <span className="App-depTime">
                         &nbsp;({nextDepLumabryggan.format("HH:mm")})
                     </span>
                 </p>
                 <p>
-                    Båten går från Barnängen {relativeTime(nextDepBarnängen)}
+                    Båten går från <strong>Barnängen</strong>{" "}
+                    {relativeTime(nextDepBarnängen)}
                     <span className="App-depTime">
                         &nbsp;({nextDepBarnängen.format("HH:mm")})
+                    </span>
+                </p>
+                <p>
+                    Båten går från <strong>Henriksdal</strong>{" "}
+                    {relativeTime(nextDepHenriksdal)}
+                    <span className="App-depTime">
+                        &nbsp;({nextDepHenriksdal.format("HH:mm")})
                     </span>
                 </p>
             </header>
