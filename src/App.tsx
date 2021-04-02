d48fa15391dea462df4bf6672441dff081c72bd1
Andreas Oldeskog <andreas.oldeskog@pricerunner.com>
2021-04-02 20:21:20 +0200

    icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,31 @@ import useForceUpdate from "./utils/hooks/useForceUpdate";
 import useInterval from "./utils/hooks/useInterval";
 
 function App() {
-    const nextDep = getNextDeparture("lumabryggan");
+    const nextDepLumabryggan = getNextDeparture("lumabryggan");
+    const nextDepBarnängen = getNextDeparture("barnängen");
     const forceUpdate = useForceUpdate();
 
     useInterval(() => {
         forceUpdate();
-        console.log("render");
     }, 1000);
 
     return (
         <div className="App">
             <header className="App-header">
+                <img src="/android-chrome-192x192.png" />
                 <h1>Sjöstadstrafiken</h1>
+
                 <p>
-                    Båten går från Lumabryggan {relativeTime(nextDep)}
+                    Båten går från Lumabryggan{" "}
+                    {relativeTime(nextDepLumabryggan)}
                     <span className="App-depTime">
-                        &nbsp;({nextDep.format("HH:mm")})
+                        &nbsp;({nextDepLumabryggan.format("HH:mm")})
+                    </span>
+                </p>
+                <p>
+                    Båten går från Barnängen {relativeTime(nextDepBarnängen)}
+                    <span className="App-depTime">
+                        &nbsp;({nextDepBarnängen.format("HH:mm")})
                     </span>
                 </p>
             </header>
