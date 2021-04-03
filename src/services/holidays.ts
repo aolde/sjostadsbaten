4e19347c47aa651211fdec089d8fef17ee2d269d
Andreas Oldeskog <andreas.oldeskog@pricerunner.com>
2021-04-03 22:19:01 +0200

    work

---
+++ src/services/holidays.ts
@@ -0,0 +1,11 @@
+const holidays = [
+    { date: "2021-01-01", holiday: "Nyårsdagen" },
+    { date: "2021-01-06", holiday: "Trettondedag jul" },
+    { date: "2021-04-02", holiday: "Långfredagen" },
+    { date: "2021-04-05", holiday: "Annandag påsk" },
+    { date: "2021-05-13", holiday: "Kristi himmelsfärdsdag" },
+    { date: "2021-06-25", holiday: "Midsommarafton" },
+    { date: "2021-12-24", holiday: "Julafton" },
+    { date: "2021-12-31", holiday: "Nyårsafton" },
+];
+export default holidays;
